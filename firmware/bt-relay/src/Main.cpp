46344b767f01dddef7bc388964f2cfaacc5f5fac
Deomid "rojer" Ryabkov <rojer@rojer.me>
2022-12-16 01:53:07 +0000

    bt-relay: Better handling of scan start/stop during OTA and reboots

--- firmware/bt-relay/src/Main.cpp
+++ firmware/bt-relay/src/Main.cpp
@@ -14,18 +14,34 @@
 
 #include "mgos_rpc.h"
 
-std::map<mgos::BTAddr, std::unique_ptr<BTSensor>> s_sensors;
+static std::map<mgos::BTAddr, std::unique_ptr<BTSensor>> s_sensors;
+static double s_scanning_since = 0;
+static bool s_reboot_immenent = false;
 
-static void StartScan() {
-  if (mgos_bt_gap_scan_in_progress()) {
-    if (mgos_ota_is_in_progress()) {
-      LOG(LL_INFO, ("Stopping scan while update is in progress"));
+static void CheckScan() {
+  bool should_scan = true;
+  if (mgos_sys_config_get_scan_duration_ms() <= 0) {
+    should_scan = false;
+  }
+  if (mgos_ota_is_in_progress()) {
+    should_scan = false;
+  }
+  if (s_reboot_immenent) {
+    should_scan = false;
+  }
+  if (!should_scan) {
+    if (mgos_bt_gap_scan_in_progress()) {
       mgos_bt_gap_scan_stop();
+    }
+    if (s_scanning_since > 0) {
+      LOG(LL_INFO, ("Stop scanning"));
+      s_scanning_since = 0;
     }
     return;
   }
-  if (mgos_sys_config_get_scan_duration_ms() <= 0) return;
-  if (mgos_ota_is_in_progress()) return;
+  if (mgos_bt_gap_scan_in_progress()) {
+    return;
+  }
   LOG(LL_DEBUG, ("Starting scan ns %d hf %d", (int) s_sensors.size(),
                  (int) mgos_get_free_heap_size()));
   struct mgos_bt_gap_scan_opts opts = {
@@ -34,7 +50,11 @@ static void StartScan() {
       .interval_ms = 0,  // Default
       .window_ms = 0,    // Default
   };
-  mgos_bt_gap_scan(&opts);
+  if (mgos_bt_gap_scan(&opts)) {
+    if (s_scanning_since == 0) {
+      s_scanning_since = mgos_uptime();
+    }
+  }
 }
 
 static void GAPHandler(int ev, void *ev_data, void *userdata) {
@@ -80,12 +100,18 @@ static void GAPHandler(int ev, void *ev_data, void *userdata) {
   (void) userdata;
 }
 
-static void StatusTimerCB() {
-  double now = mgos_uptime();
+static void CheckSensors() {
+  const double now = mgos_uptime();
   size_t num_packets = 0, total_size = 0;
   std::string packet;
   const size_t kMaxPackets = mgos_sys_config_get_max_packets();
   const size_t kMaxPacketSize = mgos_sys_config_get_max_packet_size();
+
+  // Make sure we've had the time since starting (or resuming) scanning
+  // to gather a few samples before reporting or removing stale sensors.
+  if (s_scanning_since == 0 || now - s_scanning_since < 30) {
+    return;
+  }
 
   auto send_packet = [&packet, &num_packets, &total_size]() {
     if (packet.empty()) return;
@@ -145,17 +171,30 @@ static void StatusTimerCB() {
     LOG(LL_INFO, ("Sent %d packets (%d bytes), hf %d", (int) num_packets,
                   (int) total_size, (int) mgos_get_free_heap_size()));
   }
-  StartScan();
+}
+
+static void StatusTimerCB() {
+  CheckScan();
+  CheckSensors();
 }
 
 static mgos::Timer s_statusTimer(StatusTimerCB);
 
+static void CommonEventCB(int ev, void *ev_data UNUSED_ARG,
+                          void *userdata UNUSED_ARG) {
+  switch (ev) {
+    case MGOS_EVENT_REBOOT:
+    case MGOS_EVENT_REBOOT_AFTER:
+      s_reboot_immenent = true;
+  }
+  CheckScan();
+}
+
 enum mgos_app_init_result mgos_app_init(void) {
   mgos_event_add_group_handler(MGOS_BT_GAP_EVENT_BASE, GAPHandler, nullptr);
-
+  mgos_event_add_handler(MGOS_EVENT_OTA_BEGIN, CommonEventCB, nullptr);
+  mgos_event_add_handler(MGOS_EVENT_REBOOT, CommonEventCB, nullptr);
+  mgos_event_add_handler(MGOS_EVENT_REBOOT_AFTER, CommonEventCB, nullptr);
   s_statusTimer.Reset(1000, MGOS_TIMER_REPEAT);
-
-  StartScan();
-
   return MGOS_APP_INIT_SUCCESS;
 }
